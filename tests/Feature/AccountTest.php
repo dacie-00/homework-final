365aa72e7a6104e85d8a61b999eae97e0a5b6adf
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-07-25 13:37:17 +0300

    Add tests for transfers with investment accounts

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -49,8 +49,72 @@ it('transfers money between two accounts', function () {
     $response->assertRedirect(route('account.index'));
     $this->followRedirects($response)->assertStatus(200);
 
-    $this->assertDatabaseHas('checking_accounts', ['iban' => 'sender', 'amount' => 900]);
-    $this->assertDatabaseHas('checking_accounts', ['iban' => 'receiver', 'amount' => 100]);
     $this->assertDatabaseHas('accounts', ['iban' => 'sender', 'amount' => 900]);
     $this->assertDatabaseHas('accounts', ['iban' => 'receiver', 'amount' => 100]);
+});
+
+it("does not transfer money to a different user's investment account", function () {
+    $senderUser = User::factory()->create();
+    $receiverUser = User::factory()->create();
+    Account::factory()->create([
+        'user_id' => $senderUser->id,
+        'iban' => 'sender',
+        'type' => 'checking',
+        'amount' => 1000,
+        'currency' => 'EUR',
+    ]);
+    Account::factory()->create([
+        'user_id' => $receiverUser->id,
+        'iban' => 'receiver',
+        'type' => 'investment',
+        'amount' => 0,
+        'currency' => 'EUR',
+    ]);
+
+    $response = $this->actingAs($senderUser)->post(
+        route('money-transfer.store'),
+        [
+            'account' => 'sender',
+            'iban' => 'receiver',
+            'name' => $receiverUser->name,
+            'amount' => 1,
+            'note' => null
+        ]
+    );
+    $response->assertSessionHasErrors('iban');
+    $this->assertDatabaseHas('accounts', ['iban' => 'sender', 'amount' => 1000]);
+    $this->assertDatabaseHas('accounts', ['iban' => 'receiver', 'amount' => 0]);
+});
+
+it("does not transfer money from investment account to different user's account", function () {
+    $senderUser = User::factory()->create();
+    $receiverUser = User::factory()->create();
+    Account::factory()->create([
+        'user_id' => $senderUser->id,
+        'iban' => 'sender',
+        'type' => 'investment',
+        'amount' => 1000,
+        'currency' => 'EUR',
+    ]);
+    Account::factory()->create([
+        'user_id' => $receiverUser->id,
+        'iban' => 'receiver',
+        'type' => 'checking',
+        'amount' => 0,
+        'currency' => 'EUR',
+    ]);
+
+    $response = $this->actingAs($senderUser)->post(
+        route('money-transfer.store'),
+        [
+            'account' => 'sender',
+            'iban' => 'receiver',
+            'name' => $receiverUser->name,
+            'amount' => 1,
+            'note' => null
+        ]
+    );
+    $response->assertSessionHasErrors('account');
+    $this->assertDatabaseHas('accounts', ['iban' => 'sender', 'amount' => 1000]);
+    $this->assertDatabaseHas('accounts', ['iban' => 'receiver', 'amount' => 0]);
 });
