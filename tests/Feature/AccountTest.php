ccbba2232b9aa3a28a9a2556b93372a00c347f34
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-07-25 13:37:01 +0300

    Update tests to match account refactor

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -8,10 +8,14 @@ it('creates a new money transfer account', function () {
 
     $response = $this->actingAs($user)->post(
         route('account.store'),
-        ['name' => 'testAccount', 'currency' => 'EUR']
+        [
+            'name' => 'testAccount',
+            'type' => 'checking',
+            'currency' => 'EUR'
+        ]
     );
 
-    $this->assertDatabaseHas('checking_accounts', ['name' => 'testAccount', 'currency' => 'EUR']);
+    $this->assertDatabaseHas('accounts', ['name' => 'testAccount', 'currency' => 'EUR']);
     $this->followRedirects($response)->assertStatus(200);
 });
 
@@ -19,24 +23,34 @@ it('transfers money between two accounts', function () {
     $user = User::factory()->create();
     Account::factory()->create([
         'user_id' => $user->id,
+        'iban' => 'sender',
+        'type' => 'checking',
         'amount' => 1000,
         'currency' => 'EUR',
-        'iban' => 'sender',
     ]);
     Account::factory()->create([
         'user_id' => $user->id,
+        'iban' => 'receiver',
+        'type' => 'checking',
         'amount' => 0,
         'currency' => 'EUR',
-        'iban' => 'receiver',
     ]);
 
     $response = $this->actingAs($user)->post(
         route('money-transfer.store'),
-        ['account' => 'sender', 'iban' => 'receiver', 'name' => $user->name, 'amount' => 1, 'note' => null]
+        [
+            'account' => 'sender',
+            'iban' => 'receiver',
+            'name' => $user->name,
+            'amount' => 1,
+            'note' => null
+        ]
     );
     $response->assertRedirect(route('account.index'));
     $this->followRedirects($response)->assertStatus(200);
 
     $this->assertDatabaseHas('checking_accounts', ['iban' => 'sender', 'amount' => 900]);
     $this->assertDatabaseHas('checking_accounts', ['iban' => 'receiver', 'amount' => 100]);
+    $this->assertDatabaseHas('accounts', ['iban' => 'sender', 'amount' => 900]);
+    $this->assertDatabaseHas('accounts', ['iban' => 'receiver', 'amount' => 100]);
 });
