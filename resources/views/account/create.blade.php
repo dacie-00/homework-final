1c71d799cab70617b22748a34c8ffb905cf6ddc9
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-07-27 00:12:16 +0300

    Only allow USD currency selection when account type is set to investment

--- resources/views/account/create.blade.php
+++ resources/views/account/create.blade.php
@@ -9,14 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <form method="POST" action="{{ route('account.store') }}">
+                    <form x-data="{type: 'checking', value: ''}" method="POST" action="{{ route('account.store') }}">
                         @csrf
                         <x-input-label for="name" :value="__('Name')"/>
                         <x-text-input id="name" name="name" value="{{ old('name') }}"></x-text-input>
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
 
                         <x-input-label for="type" :value="__('Account type')"/>
-                        <x-select id="type" name="type">
+                        <x-select x-model='type' id="type" name="type">
                             <option value="checking" selected>Checking</option>
                             <option value="investment" selected>Investment</option>
                         </x-select>
@@ -24,10 +24,10 @@
 
 {{--                    TODO: disable currency selection when investment type is selected (probably use js)--}}
                         <x-input-label for="currency" :value="__('Currency')"/>
-                        <x-select id="currency" name="currency">
+                        <x-select x-bind:disabled="type == 'investment'" id="currency" name="currency">
                             <option selected>Select a currency</option>
                             @foreach($currencies as $currency)
-                                <option value="{{ $currency }}">
+                                <option :selected="type === 'investment' && $el.value === 'USD'" value="{{ $currency }}">
                                     {{ $currency }}
                                 </option>
                             @endforeach
