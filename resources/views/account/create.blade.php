c0f37dddebd06e47240ba537c18d1d384e855457
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-07-29 21:23:56 +0300

    Fix investment account select input not submitting

--- resources/views/account/create.blade.php
+++ resources/views/account/create.blade.php
@@ -6,31 +6,35 @@
     </x-slot>
 
     <x-content>
-        <form x-data="{type: 'checking', value: ''}" method="POST" action="{{ route('account.store') }}">
+        <form class="space-y-6" x-data="{type: 'checking', value: ''}" method="POST" action="{{ route('account.store') }}">
             @csrf
-            <x-input-label for="name" :value="__('Name')"/>
-            <x-text-input id="name" name="name" value="{{ old('name') }}"></x-text-input>
-            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+            <div>
+                <x-input-label for="name" :value="__('Name')"/>
+                <x-text-input id="name" name="name" value="{{ old('name') }}"></x-text-input>
+                <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+            </div>
 
-            <x-input-label for="type" :value="__('Account type')"/>
-            <x-select x-model='type' id="type" name="type">
-                <option value="checking" selected>Checking</option>
-                <option value="investment" selected>Investment</option>
-            </x-select>
-            <x-input-error :messages="$errors->get('type')" class="mt-2"/>
+            <div>
+                <x-input-label for="type" :value="__('Account type')"/>
+                <x-select x-model='type' id="type" name="type">
+                    <option value="checking" selected>Checking</option>
+                    <option value="investment" selected>Investment</option>
+                </x-select>
+                <x-input-error :messages="$errors->get('type')" class="mt-2"/>
+            </div>
 
-            {{--                    TODO: disable currency selection when investment type is selected (probably use js)--}}
-            <x-input-label for="currency" :value="__('Currency')"/>
-            <x-select x-bind:disabled="type == 'investment'" id="currency" name="currency">
-                <option selected>Select a currency</option>
-                @foreach($currencies as $currency)
-                    <option :selected="type === 'investment' && $el.value === 'USD'" value="{{ $currency }}">
-                        {{ $currency }}
-                    </option>
-                @endforeach
-            </x-select>
-            <x-input-error :messages="$errors->get('currency')" class="mt-2"/>
-            <br>
+            <div>
+                <x-input-label for="currency" :value="__('Currency')"/>
+                <x-select id="currency" name="currency">
+                    <option selected>Select a currency</option>
+                    @foreach($currencies as $currency)
+                        <option :disabled="type === 'investment' && $el.value !== 'USD'" :selected="type === 'investment' && $el.value === 'USD'" value="{{ $currency }}">
+                            {{ $currency }}
+                        </option>
+                    @endforeach
+                </x-select>
+                <x-input-error :messages="$errors->get('currency')" class="mt-2"/>
+            </div>
             <x-primary-button>{{ __('Submit') }}</x-primary-button>
         </form>
     </x-content>
