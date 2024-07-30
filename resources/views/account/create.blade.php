d04c502dcecd986801ba87ed8d6ad14a3303896b
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-07-30 13:35:48 +0300

    Fix spacing bank and transfer form views and add titles

--- resources/views/account/create.blade.php
+++ resources/views/account/create.blade.php
@@ -6,36 +6,45 @@
     </x-slot>
 
     <x-content>
-        <form class="space-y-6" x-data="{type: 'checking', value: ''}" method="POST" action="{{ route('account.store') }}">
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Create a new bank account') }}
+        </h2>
+
+        <p class="mt-1 mb-6 text-sm text-gray-600">
+            {{ __('Note: that investment accounts can only have USD as the account currency.') }}
+        </p>
+        <form x-data="{type: 'checking', value: ''}" method="POST" action="{{ route('account.store') }}">
             @csrf
-            <div>
-                <x-input-label for="name" :value="__('Name')"/>
-                <x-text-input id="name" name="name" value="{{ old('name') }}"></x-text-input>
-                <x-input-error :messages="$errors->get('name')" class="mt-2"/>
-            </div>
+            <div class="space-y-6">
+                <div>
+                    <x-input-label for="name" :value="__('Name')"/>
+                    <x-text-input id="name" name="name" value="{{ old('name') }}"></x-text-input>
+                    <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+                </div>
 
-            <div>
-                <x-input-label for="type" :value="__('Account type')"/>
-                <x-select x-model='type' id="type" name="type">
-                    <option value="checking" selected>Checking</option>
-                    <option value="investment" selected>Investment</option>
-                </x-select>
-                <x-input-error :messages="$errors->get('type')" class="mt-2"/>
-            </div>
+                <div>
+                    <x-input-label for="type" :value="__('Account type')"/>
+                    <x-select x-model='type' id="type" name="type">
+                        <option value="checking" selected>Checking</option>
+                        <option value="investment" selected>Investment</option>
+                    </x-select>
+                    <x-input-error :messages="$errors->get('type')" class="mt-2"/>
+                </div>
 
-            <div>
-                <x-input-label for="currency" :value="__('Currency')"/>
-                <x-select id="currency" name="currency">
-                    <option selected>Select a currency</option>
-                    @foreach($currencies as $currency)
-                        <option :disabled="type === 'investment' && $el.value !== 'USD'" :selected="type === 'investment' && $el.value === 'USD'" value="{{ $currency }}">
-                            {{ $currency }}
-                        </option>
-                    @endforeach
-                </x-select>
-                <x-input-error :messages="$errors->get('currency')" class="mt-2"/>
+                <div>
+                    <x-input-label for="currency" :value="__('Currency')"/>
+                    <x-select id="currency" name="currency">
+                        <option selected>Select a currency</option>
+                        @foreach($currencies as $currency)
+                            <option :disabled="type === 'investment' && $el.value !== 'USD'" :selected="type === 'investment' && $el.value === 'USD'" value="{{ $currency }}">
+                                {{ $currency }}
+                            </option>
+                        @endforeach
+                    </x-select>
+                    <x-input-error :messages="$errors->get('currency')" class="mt-2"/>
+                </div>
+                <x-primary-button>{{ __('Submit') }}</x-primary-button>
             </div>
-            <x-primary-button>{{ __('Submit') }}</x-primary-button>
         </form>
     </x-content>
 </x-app-layout>
--- resources/views/money-transfer/create.blade.php
+++ resources/views/money-transfer/create.blade.php
@@ -6,40 +6,54 @@
     </x-slot>
 
     <x-content>
+        <h2 class="text-lg font-medium text-gray-900 mb-6">
+            {{ __('Make a new money transfer') }}
+        </h2>
+
         <form method="POST" action="{{ route('money-transfer.store') }}" id="transfer-form">
             @csrf
-            <x-input-label for="account" :value="__('Account to make transfer from')"/>
-            <x-select id="account" name="account">
-                <option selected>Choose an account</option>
-                @foreach($accounts as $account)
-                    <option value="{{ $account->iban }}">
-                        {{
-                            $account->name . ' (' .
-                            number_format($account->amount / 100, 2) . ' ' .
-                            $account->currency . ')'
-                        }}
-                    </option>
-                @endforeach
-            </x-select>
-            <x-input-error :messages="$errors->get('account')" class="mt-2"/>
-            <x-input-label for="iban" :value="__('Receiver account IBAN')"/>
-            <x-text-input id="iban" name="iban" value="{{ old('iban') }}"></x-text-input>
-            <x-input-error :messages="$errors->get('iban')" class="mt-2"/>
-
-            <x-input-label for="name" :value="__('Receiver name')"/>
-            <x-text-input id="name" name="name" value="{{ old('name') }}"></x-text-input>
-            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
-
-            <x-input-label for="amount" :value="__('Amount')"/>
-            <x-text-input type="number" step="0.01" id="amount" name="amount"
-                          value="{{ old('amount') }}"></x-text-input>
-            <x-input-error :messages="$errors->get('amount')" class="mt-2"/>
-
-            <x-input-label for="note" :value="__('Note')"/>
-            <x-text-input id="note" name="note" value="{{ old('note') }}"></x-text-input>
-            <x-input-error :messages="$errors->get('note')" class="mt-2"/>
-            <br>
-            <x-primary-button>{{ __('Submit') }}</x-primary-button>
+            <div class="space-y-6">
+                <div>
+                    <x-input-label for="account" :value="__('Account to make transfer from')"/>
+                    <x-select id="account" name="account">
+                        <option selected>Choose an account</option>
+                        @foreach($accounts as $account)
+                            <option value="{{ $account->iban }}">
+                                {{
+                                    $account->name . ' (' .
+                                    number_format($account->amount / 100, 2) . ' ' .
+                                    $account->currency . ')'
+                                }}
+                            </option>
+                        @endforeach
+                    </x-select>
+                    <x-input-error :messages="$errors->get('account')" class="mt-2"/>
+                </div>
+                <div>
+                    <x-input-label for="iban" :value="__('Receiver account IBAN')"/>
+                    <x-text-input id="iban" name="iban" value="{{ old('iban') }}"></x-text-input>
+                    <x-input-error :messages="$errors->get('iban')" class="mt-2"/>
+                </div>
+                <div>
+                    <x-input-label for="name" :value="__('Receiver name')"/>
+                    <x-text-input id="name" name="name" value="{{ old('name') }}"></x-text-input>
+                    <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+                </div>
+                <div>
+                    <x-input-label for="amount" :value="__('Amount')"/>
+                    <x-text-input type="number" step="0.01" id="amount" name="amount"
+                                  value="{{ old('amount') }}"></x-text-input>
+                    <x-input-error :messages="$errors->get('amount')" class="mt-2"/>
+                </div>
+                <div>
+                    <x-input-label for="note" :value="__('Note')"/>
+                    <x-text-input id="note" name="note" value="{{ old('note') }}"></x-text-input>
+                    <x-input-error :messages="$errors->get('note')" class="mt-2"/>
+                </div>
+                <div>
+                    <x-primary-button>{{ __('Submit') }}</x-primary-button>
+                </div>
+            </div>
         </form>
     </x-content>
 </x-app-layout>
